448d64ff5a1fbe4a6ce42e116a6d278e0540d341
Luke Lai <lukelai.dev@gmail.com>
2022-10-17 16:09:55 +0100

    delete comments

--- script.js
+++ script.js
@@ -25,23 +25,6 @@ const toggleDarkMode = (isDark) => {
   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
   isDark ? imageMode("dark") : imageMode("light");
 };
-// // Dark Mode Styles
-// const darkMode = () => {
-//   nav.style.backgroundColor = "rgb(0 0 0/50%)";
-//   textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
-//   toggleIcon.children[0].textContent = "Dark Mode";
-//   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-//   imageMode("dark");
-// };
-
-// // Light Mode Styles
-// const lightMode = () => {
-//   nav.style.backgroundColor = "rgb(255 255 255/50%)";
-//   textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
-//   toggleIcon.children[0].textContent = "Dark Mode";
-//   toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-//   imageMode("light");
-// };
 
 // Switch Theme Dynamically
 const switchTheme = (event) => {
